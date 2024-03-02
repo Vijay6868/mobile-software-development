8e895dc5a4b317d2ecda7460e94ce967c198ba12
Vijay Kumar <saroyav3@gmail.com>
2024-03-02 22:49:51 +1300

    working on ex3

---
+++ week-1/ex3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onBtnPlus(View view){
+        TextView txtOp = findViewById(R.id.lbOperator);
+        txtOp.setText("+");
+
+
+    }
+}
