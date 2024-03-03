fac6dc735c91e67d2dc7c66c996f5373a06fd44a
Vijay Kumar <saroyav3@gmail.com>
2024-03-03 21:32:47 +1300

    finished with ex3

--- week-1/ex3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ week-1/ex3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,6 +18,52 @@ public class MainActivity extends AppCompatActivity {
         TextView txtOp = findViewById(R.id.lbOperator);
         txtOp.setText("+");
 
+        TextView num1 = findViewById(R.id.tbNum1);
+        TextView num2 = findViewById(R.id.tbNum2);
+        TextView result = findViewById(R.id.tbResult);
 
+
+        int value1 = Integer.parseInt(num1.getText().toString());
+        int value2 = Integer.parseInt(num2.getText().toString());
+
+        int value3 = value1+value2;
+
+        result.setText(String.valueOf(value3));
+
+
+    }
+    public void onBtnMinus(View view) {
+        TextView txtOp = findViewById(R.id.lbOperator);
+        txtOp.setText("-");
+
+
+        TextView num1 = findViewById(R.id.tbNum1);
+        TextView num2 = findViewById(R.id.tbNum2);
+        TextView result = findViewById(R.id.tbResult);
+
+
+        int value1 = Integer.parseInt(num1.getText().toString());
+        int value2 = Integer.parseInt(num2.getText().toString());
+
+        int value3 = value1-value2;
+
+        result.setText(String.valueOf(value3));
+    }
+    public void onBtnMulti(View view) {
+        TextView txtOp = findViewById(R.id.lbOperator);
+        txtOp.setText("x");
+
+
+        TextView num1 = findViewById(R.id.tbNum1);
+        TextView num2 = findViewById(R.id.tbNum2);
+        TextView result = findViewById(R.id.tbResult);
+
+
+        int value1 = Integer.parseInt(num1.getText().toString());
+        int value2 = Integer.parseInt(num2.getText().toString());
+
+        int value3 = value1*value2;
+
+        result.setText(String.valueOf(value3));
     }
 }
