3415377e92097d67e14cd78b683b1c7b0e063238
Vijay Kumar <saroyav3@gmail.com>
2024-03-03 21:49:09 +1300

    finished with ex3 improved_code

---
+++ week-1/ex3_improved_code/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void performOperation(String operator) {
+        TextView txtOp = findViewById(R.id.lbOperator);
+        txtOp.setText(operator);
+
+        TextView num1 = findViewById(R.id.tbNum1);
+        TextView num2 = findViewById(R.id.tbNum2);
+        TextView result = findViewById(R.id.tbResult);
+
+        int value1 = Integer.parseInt(num1.getText().toString());
+        int value2 = Integer.parseInt(num2.getText().toString());
+
+        int value3;
+        switch (operator) {
+            case "+":
+                value3 = value1 + value2;
+                break;
+            case "-":
+                value3 = value1 - value2;
+                break;
+            case "x":
+                value3 = value1 * value2;
+                break;
+            default:
+                // Handle invalid operator here if necessary
+                return;
+        }
+
+        result.setText(String.valueOf(value3));
+    }
+
+    public void onBtnPlus(View view) {
+        performOperation("+");
+    }
+
+    public void onBtnMinus(View view) {
+        performOperation("-");
+    }
+
+    public void onBtnMulti(View view) {
+        performOperation("x");
+    }
+
+}
