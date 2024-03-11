0fe06a4e3543145d7a418a7dfe45c23e244a44de
Vijay Kumar <saroyav3@gmail.com>
2024-03-11 15:19:08 +1300

    still working

--- week3/ex3_improved_code/app/src/main/java/com/example/myapplication/MainActivity.java
+++ week3/ex3_improved_code/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,11 +9,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    View OP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View btnPlus = findViewById(R.id.plusButton);
+        View btnMinus = findViewById(R.id.minusButton);
+        View btnMulti = findViewById(R.id.multiButton);
+
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnMulti.setOnClickListener(this);
+
+        //this.OP = findViewById(R.id.lbOperator);
+        //txtOp.setText(operator);
     }
     public void performOperation(String operator) {
         TextView txtOp = findViewById(R.id.lbOperator);
@@ -22,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         TextView num1 = findViewById(R.id.tbNum1);
         TextView num2 = findViewById(R.id.tbNum2);
         TextView result = findViewById(R.id.tbResult);
+
+
 
         int value1 = Integer.parseInt(num1.getText().toString());
         int value2 = Integer.parseInt(num2.getText().toString());
@@ -61,5 +74,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        TextView num1 = findViewById(R.id.tbNum1);
+        TextView num2 = findViewById(R.id.tbNum2);
+        TextView result = findViewById(R.id.tbResult);
+
+        int value1 = Integer.parseInt(num1.getText().toString());
+        int value2 = Integer.parseInt(num2.getText().toString());
+
+        }
     }
 }
