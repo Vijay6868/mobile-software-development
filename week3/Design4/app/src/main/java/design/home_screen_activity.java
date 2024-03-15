1d84f1a2497956b52edcd6130374c153c7dfbd37
Vijay Kumar <saroyav3@gmail.com>
2024-03-15 14:27:48 +1300

    working on Kids_arithmatic_game

---
+++ week3/Design4/app/src/main/java/design/home_screen_activity.java
@@ -0,0 +1,61 @@
+package design;
+
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		home_screen
+	 *	@date 		Wednesday 13th of March 2024 10:55:37 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class home_screen_activity extends Activity {
+
+	
+	private View _bg__home_screen_ek2;
+	private TextView kids_arithmetic_game;
+	private View rectangle_1;
+	private View rectangle_2;
+	private View rectangle_3;
+	private View rectangle_4;
+	private TextView enter_your_name;
+	private TextView year;
+	private TextView operator;
+	private TextView start_game;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_screen);
+
+		
+		_bg__home_screen_ek2 = (View) findViewById(R.id._bg__home_screen_ek2);
+		kids_arithmetic_game = (TextView) findViewById(R.id.kids_arithmetic_game);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		enter_your_name = (TextView) findViewById(R.id.enter_your_name);
+		year = (TextView) findViewById(R.id.year);
+		operator = (TextView) findViewById(R.id.operator);
+		start_game = (TextView) findViewById(R.id.start_game);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ week3/ExportDesign3/app/src/main/java/exportkit/figma/home_screen_activity.java
@@ -0,0 +1,130 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		home_screen
+	 *	@date 		Thursday 14th of March 2024 09:35:27 AM
+	 *	@title 		Deal App
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class home_screen_activity extends Activity {
+
+	
+	private View _bg__home_screen_ek2;
+	private View _bg__csearchbar_ek1;
+	private View search;
+	private View search_ek1;
+	private ImageView vector;
+	private View _bg__booking_card_ek1;
+	private View trip_card;
+	private ImageView _5209_image2_milfordcruise4_1;
+	private TextView pure_milford_sound_cruise;
+	private TextView tap_for_more_details;
+	private TextView __99;
+	private View _bg__rating_ek1;
+	private ImageView star_1;
+	private ImageView star_2;
+	private ImageView star_3;
+	private ImageView star_4;
+	private ImageView star_5;
+	private View _bg__cnavbar_ek1;
+	private View rectangle_4;
+	private ImageView homeicon;
+	private View _bg__profileicon_ek1;
+	private ImageView vector_ek1;
+	private ImageView vector_ek2;
+	private View _bg__categories_icon_ek1;
+	private ImageView vector_ek3;
+	private ImageView vector_ek4;
+	private ImageView categorisicon;
+	private View _bg__booking_card_ek3;
+	private View trip_card_ek1;
+	private ImageView _5209_image2_milfordcruise4_1_ek1;
+	private TextView pure_milford_sound_cruise_ek1;
+	private TextView tap_for_more_details_ek1;
+	private TextView __99_ek1;
+	private View _bg__rating_ek3;
+	private ImageView star_1_ek1;
+	private ImageView star_2_ek1;
+	private ImageView star_3_ek1;
+	private ImageView star_4_ek1;
+	private ImageView star_5_ek1;
+	private View rectangle_10;
+	private TextView featured;
+	private TextView categories;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_screen);
+
+		
+		_bg__home_screen_ek2 = (View) findViewById(R.id._bg__home_screen_ek2);
+		_bg__csearchbar_ek1 = (View) findViewById(R.id._bg__csearchbar_ek1);
+		search = (View) findViewById(R.id.search);
+		search_ek1 = (View) findViewById(R.id.search_ek1);
+		vector = (ImageView) findViewById(R.id.vector);
+		_bg__booking_card_ek1 = (View) findViewById(R.id._bg__booking_card_ek1);
+		trip_card = (View) findViewById(R.id.trip_card);
+		_5209_image2_milfordcruise4_1 = (ImageView) findViewById(R.id._5209_image2_milfordcruise4_1);
+		pure_milford_sound_cruise = (TextView) findViewById(R.id.pure_milford_sound_cruise);
+		tap_for_more_details = (TextView) findViewById(R.id.tap_for_more_details);
+		__99 = (TextView) findViewById(R.id.__99);
+		_bg__rating_ek1 = (View) findViewById(R.id._bg__rating_ek1);
+		star_1 = (ImageView) findViewById(R.id.star_1);
+		star_2 = (ImageView) findViewById(R.id.star_2);
+		star_3 = (ImageView) findViewById(R.id.star_3);
+		star_4 = (ImageView) findViewById(R.id.star_4);
+		star_5 = (ImageView) findViewById(R.id.star_5);
+		_bg__cnavbar_ek1 = (View) findViewById(R.id._bg__cnavbar_ek1);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		homeicon = (ImageView) findViewById(R.id.homeicon);
+		_bg__profileicon_ek1 = (View) findViewById(R.id._bg__profileicon_ek1);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		_bg__categories_icon_ek1 = (View) findViewById(R.id._bg__categories_icon_ek1);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		categorisicon = (ImageView) findViewById(R.id.categorisicon);
+		_bg__booking_card_ek3 = (View) findViewById(R.id._bg__booking_card_ek3);
+		trip_card_ek1 = (View) findViewById(R.id.trip_card_ek1);
+		_5209_image2_milfordcruise4_1_ek1 = (ImageView) findViewById(R.id._5209_image2_milfordcruise4_1_ek1);
+		pure_milford_sound_cruise_ek1 = (TextView) findViewById(R.id.pure_milford_sound_cruise_ek1);
+		tap_for_more_details_ek1 = (TextView) findViewById(R.id.tap_for_more_details_ek1);
+		__99_ek1 = (TextView) findViewById(R.id.__99_ek1);
+		_bg__rating_ek3 = (View) findViewById(R.id._bg__rating_ek3);
+		star_1_ek1 = (ImageView) findViewById(R.id.star_1_ek1);
+		star_2_ek1 = (ImageView) findViewById(R.id.star_2_ek1);
+		star_3_ek1 = (ImageView) findViewById(R.id.star_3_ek1);
+		star_4_ek1 = (ImageView) findViewById(R.id.star_4_ek1);
+		star_5_ek1 = (ImageView) findViewById(R.id.star_5_ek1);
+		rectangle_10 = (View) findViewById(R.id.rectangle_10);
+		featured = (TextView) findViewById(R.id.featured);
+		categories = (TextView) findViewById(R.id.categories);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
